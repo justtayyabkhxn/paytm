dca2543473a9bf4cfe0ae9f40f147f824adfb34c
Tayyab Khan <tayyabkhangk4734@gmail.com>
2025-01-18 12:23:45 +0530

    backend routes

---
+++ backend/db.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firsName: {
+        type: String,
+        required: true,
+        maxLength: 50,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 50,
+        trim: true
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.Objectid,
+        ref: "User",
+        required: true
+    },
+    balance: ({
+        type: Number,
+        required: true
+    })
+
+})
+
+const Account = mongoose.model("Account", accountSchema);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User,
+    Account
+};
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const rootRouter = require("./routes/index");
+const cors = require("cors");
 
-const app=express();
 
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.use("/app/v1", rootRouter);
+
+app.listen(3000)
 
